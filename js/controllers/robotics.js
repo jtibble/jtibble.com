385a3a80e867ce95e9b68ae4967ae9e037c300d0
jtibble <john@jtibble.com>
2013-09-21 11:56:20 -0400

    Updated gifList.html formatting, added titles

--- js/controllers/robotics.js
+++ js/controllers/robotics.js
@@ -9,31 +9,37 @@ define(['text!templates/layouts/gifList.html',
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=MtgnZGaljbg',
 					'imageURL': 'img/robotics/robots.gif',
+					'title': '',
 					'text': 'MAGIC 2010 Autonomous Robotics Competition'
 				},	
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=AcvheysmuvE',
 					'imageURL': 'img/robotics/peopleTracker.gif',
+					'title': '',
 					'text': 'LIDAR-based Person-Tracker'
 				},	
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=0DDX7VSyCq0',
 					'imageURL': 'img/robotics/odometry.gif',
+					'title': '',
 					'text': 'Building a LIDAR map from pure odometry, collected on each wheel and integrated'
 				},
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=P3-UXN2_yto',
 					'imageURL': 'img/robotics/competition.gif',
+					'title': '',
 					'text': 'Robotics class-competition, to follow the line using the tag-tracking data provided by a ceiling-mounted camera'
 				},
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=TyW1BPpHy18',
 					'imageURL': 'img/robotics/bicycle.gif',
+					'title': '',
 					'text': '"Bicycle" driving algorithm simulation'
 				},
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=uY96ye92m2s',
 					'imageURL': 'img/robotics/robotArm.gif',
+					'title': '',
 					'text': 'Arm uses Jacobian of joint angles to move, overhead camera to identify targets'
 				}				
 			];
--- js/views/umichView.js
+++ js/views/umichView.js
@@ -1,4 +1,8 @@
-define(['text!templates/umich.html', 'text!templates/layouts/gifList.html'], function( template, gifTemplate ){
+define(['text!templates/layouts/gifListWrapper.html', 
+		'text!templates/layouts/gifList.html'], 
+	   function( giftTemplateWrapperTemplate, 
+				 gifListTemplate ){
+		   
 	var view = Backbone.View.extend({
 		initialize: function(){
 			this.render();
@@ -12,28 +16,32 @@ define(['text!templates/umich.html', 'text!templates/layouts/gifList.html'], fun
 				{
 					'destinationURL': '#umich/TNB',
 					'imageURL': 'img/umich/ThrashNBurnSmall.png',
+					'title': 'Thrash N Burn',
 					'text': '"Thrash \'N\' Burn", an OpenGL 3D racing/combat game for Senior Design'
 				},			
 				{
 					'destinationURL': '#graphics',
 					'imageURL': 'img/umich/fish.gif',
+					'title': '',
 					'text': 'Computer Graphics Projects'
 				},			
 				{
 					'destinationURL': '#robotics',
 					'imageURL': 'img/robotics/robots.gif',
+					'title': '',
 					'text': 'APRIL Robotics Lab'
 				},			
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=J7tMyShg9-A',
 					'imageURL': 'img/umich/pursuitRevealShort.gif',
+					'title': '',
 					'text': '"Pursuit", a live-action special-effects-demo chase scene'
 				}
 			];
 			
-			var renderedGIFTemplate = _.template( gifTemplate, {'model': {'items': list}} );
+			var renderedGIFTemplate = _.template( gifListTemplate, {'model': {'items': list}} );
 			
-			var renderedTemplate = _.template( template, {'model': {'gifList': renderedGIFTemplate}} );
+			var renderedTemplate = _.template( giftTemplateWrapperTemplate, {'model': {'gifList': renderedGIFTemplate}} );
 			
 			$( this.options.el ).html( renderedTemplate );
 		}
