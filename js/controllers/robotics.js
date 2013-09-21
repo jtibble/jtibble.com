3cfc7b7c1fd20ca2b9f8a4119b2de7dc0d32794e
jtibble <john@jtibble.com>
2013-09-21 13:00:40 -0400

    Added lots of content to the umichView.js and robotics pages. Updated gifList.html for better layout.

--- js/controllers/robotics.js
+++ js/controllers/robotics.js
@@ -9,38 +9,38 @@ define(['text!templates/layouts/gifList.html',
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=MtgnZGaljbg',
 					'imageURL': 'img/robotics/robots.gif',
-					'title': '',
-					'text': 'MAGIC 2010 Autonomous Robotics Competition'
+					'title': 'MAGIC 2010 Autonomous Robotics Competition',
+					'text': 'The University of Michigan\'s April Robotics Laboratory participated in the MAGIC 2010 competition, passing through all qualification rounds, and finally winning the final round of competition in Australia. I am proud to have worked in the robotics lab with Dr. Edwin Olson and my fellow students, building and testing the hardware and software for this competition. This video shows our fleet of robots in-action for the judges\' site visit.'
 				},	
 				{
-					'destinationURL': 'http://www.youtube.com/watch?v=AcvheysmuvE',
+					'destinationURL': 'http://youtu.be/AcvheysmuvE?t=13m5s',
 					'imageURL': 'img/robotics/peopleTracker.gif',
-					'title': '',
-					'text': 'LIDAR-based Person-Tracker'
+					'title': 'LIDAR-based Person-Tracker',
+					'text': 'I built a system to allow snooping robots to track "mobile objects of interest", even as they were occluded by other people or static objects. The system estimates the speed of each tracked person to attempt to re-aquire them once they re-emerge from behind the obstacle.'
 				},	
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=0DDX7VSyCq0',
 					'imageURL': 'img/robotics/odometry.gif',
-					'title': '',
-					'text': 'Building a LIDAR map from pure odometry, collected on each wheel and integrated'
-				},
-				{
-					'destinationURL': 'http://www.youtube.com/watch?v=P3-UXN2_yto',
-					'imageURL': 'img/robotics/competition.gif',
-					'title': '',
-					'text': 'Robotics class-competition, to follow the line using the tag-tracking data provided by a ceiling-mounted camera'
+					'title': 'Odometry Mapping',
+					'text': 'Using only the encoders built into each the robots\' four wheels, I built a system to more accurately estimate the position and motion of the robot as it completed a circuit of our building. This map could be used as a SLAM prior to dramatically improve the quality of generated maps. In this video, the robot travels roughly 60 meters.'
 				},
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=TyW1BPpHy18',
 					'imageURL': 'img/robotics/bicycle.gif',
-					'title': '',
-					'text': '"Bicycle" driving algorithm simulation'
+					'title': '"Bicycle" Driving Algorithm',
+					'text': 'As part of my robotics class, my team and I developed this bicycle driving algorithm to plan the most efficient route along a path given a look-ahead constraint.'
+				},
+				{
+					'destinationURL': 'http://www.youtube.com/watch?v=P3-UXN2_yto',
+					'imageURL': 'img/robotics/competition.gif',
+					'title': 'Robotics Class Competition',
+					'text': 'Used the above bicycle driving algorithm to follow the path. Odometry was provided by ceiling-mounted camera and tag-recognition software.'
 				},
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=uY96ye92m2s',
 					'imageURL': 'img/robotics/robotArm.gif',
-					'title': '',
-					'text': 'Arm uses Jacobian of joint angles to move, overhead camera to identify targets'
+					'title': 'Computer Vision and 3D Motion Planning',
+					'text': 'An overhead camera looking down at the robotic arm detects the yellow balls (via HSV threshholding and image segmentation). The location of each ball is projected into arm-space through a camera calibration (using the white marks). The location of the robot\'s end-effector in 3D space is calculated as a function of the joint angles and lengths of each arm-segment. Our algorithm generates kinematically-reachable random points around the robot, and selects a path from point-to-point (Rapidly Exploring Randomized Tree, or RRT algorithm) that brings the end-effector towards the target (decending using a Jacobian and numerical integration). Once the arm has reached the target, the grippers close, and the robot plots a new route to the bowl to deliver the ball, and the process repeats.'
 				}				
 			];
 			
--- js/views/umichView.js
+++ js/views/umichView.js
@@ -17,25 +17,25 @@ define(['text!templates/layouts/gifListWrapper.html',
 					'destinationURL': '#umich/TNB',
 					'imageURL': 'img/umich/ThrashNBurnSmall.png',
 					'title': 'Thrash N Burn',
-					'text': '"Thrash \'N\' Burn", an OpenGL 3D racing/combat game for Senior Design'
+					'text': 'An OpenGL racing/combat game, built as part of my Senior Design class at U of M\'s College of Engineering. The game features a 3D model-loader (with textures), GLSL shaders, physics simulation, particle system, enemy AI, multiple weapons, menu-system, and original music by Evan Perry.'
 				},			
 				{
 					'destinationURL': '#graphics',
 					'imageURL': 'img/umich/fish.gif',
-					'title': '',
-					'text': 'Computer Graphics Projects'
+					'title': 'Computer Graphics Projects',
+					'text': 'This from-scratch GLSL skin shader won the "Best Shader" competition in my Computer Graphics class<br><br>Click through to look at more Computer Graphics projects'
 				},			
 				{
 					'destinationURL': '#robotics',
 					'imageURL': 'img/robotics/robots.gif',
-					'title': '',
-					'text': 'APRIL Robotics Lab'
+					'title': 'APRIL Robotics Laboratory',
+					'text': 'I\'ve worked on many projects as part of the robotics team here on campus. Click through to check them out.'
 				},			
 				{
 					'destinationURL': 'http://www.youtube.com/watch?v=J7tMyShg9-A',
 					'imageURL': 'img/umich/pursuitRevealShort.gif',
-					'title': '',
-					'text': '"Pursuit", a live-action special-effects-demo chase scene'
+					'title': '"Pursuit"',
+					'text': 'Directed a special-effects action-scene as part of my Visual Effects class. Once filming was complete, I designed, rendered, and composited the Orb into several of the shots. Effects were built with After Effects and its Mocha tracker.<br><br>Click to see the video in its entirety.'
 				}
 			];
 			
