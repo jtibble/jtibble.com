c96e28577bac1519fde2efee45213a8c458ed8d3
jtibble <john@jtibble.com>
2013-11-30 18:19:13 -0500

    Added click-handlers for each of the wedding links.

--- js/views/weddingView.js
+++ js/views/weddingView.js
@@ -1,5 +1,13 @@
 define(['text!templates/wedding/wedding.html'], function( template ){
 	var view = Backbone.View.extend({
+		events: {
+			'click #menuItemBoxStory': 		'showStory',
+			'click #menuItemBoxCeremony': 	'showCeremony',
+			'click #menuItemBoxParty': 		'showParty',
+			'click #menuItemBoxRegistry': 	'showRegistry',
+			'click #menuItemBoxInfo': 		'showInfo',
+			'click #menuItemBoxPhotos': 	'showPhotos',
+		},
 		initialize: function(){
 			this.render();
 		},
@@ -9,6 +17,24 @@ define(['text!templates/wedding/wedding.html'], function( template ){
 			}
 			
 			$( this.options.el ).html( template );
+		},
+		showStory: function(){
+			console.log('show story');	
+		},
+		showCeremony: function(){
+			console.log('show ceremony');	
+		},
+		showParty: function(){
+			console.log('show party');	
+		},
+		showRegistry: function(){
+			console.log('show registry');	
+		},
+		showInfo: function(){
+			console.log('show info');	
+		},
+		showPhotos: function(){
+			console.log('show photos');	
 		}
 	});
 									
