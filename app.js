8cfdf96e56344b7b4ce1466c17e87f24bcf94edf
jtibble <john@jtibble.com>
2015-04-16 10:11:30 -0400

    Home page shifted to Angular

---
+++ app.js
@@ -0,0 +1,35 @@
+var app = angular.module('app', ['ui.router', 'ui.bootstrap']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise("/home");
+    
+    $stateProvider.state('home', {
+        url: "/home",
+        templateUrl: "templates/home.html"
+    });
+});
+
+app.provider('FrameworkAJAX', function(){
+	return {
+		$get: ['$http', function( $http ){
+			return {
+				sendRequest: function(request, successCallback, errorCallback){
+					
+					if( !request.method || !request.url || !request.data ){
+						console.log('Error making AJAX request: missing method, url, or data.');
+						return;
+					}
+					
+					return $http( request ).success( successCallback ).error( errorCallback );
+				}
+			};
+		}]		
+	};
+});
+
+app.directive('header', function(){
+  return {
+    templateUrl: 'templates/headers/headerHomeTemplate.html'
+  };
+});
